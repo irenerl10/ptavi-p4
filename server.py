4f5ca437888300afa1fbd9f7fddedaaf3c8d856b
irenerl10 <irene17a@gmail.com>
2018-10-27 20:06:10 +0200

    imprime IP y port client y me lee port de sheel

--- server.py
+++ server.py
@@ -5,7 +5,11 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
 
+if len(sys.argv) != 2:
+	sys.exit('Usage: python3 seerver.py port')
+PORT = int(sys.argv[1])
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -19,12 +23,17 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         """
         self.wfile.write(b"Hemos recibido tu peticion")
         for line in self.rfile:
+			#como consigo IP y puerto cliente sin mandarselo desde cliente??
+            IP_CLIENT = line.decode('utf-8').split()[0]
+            PORT_CLIENT = line.decode('utf-8').split()[1]
+            print(IP_CLIENT, PORT_CLIENT)
             print("El cliente nos manda ", line.decode('utf-8'))
+	
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001 
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', 6001), EchoHandler) 
+    serv = socketserver.UDPServer(('', PORT), EchoHandler) 
 
     print("Lanzando servidor UDP de eco...")
     try:
