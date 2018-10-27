243d74127a8a2ed1d1b1ec45c04a3d1419f44b1a
irenerl10 <irene17a@gmail.com>
2018-10-27 21:41:54 +0200

    Ya consegui poner bien la ip y port

--- client.py
+++ client.py
@@ -12,13 +12,16 @@ SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 LINE = ' '.join(sys.argv[3:])
 
-
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     print("Enviando:", LINE)
-
-    my_socket.send(bytes(SERVER+ ' ' +(str(PORT))+' '+ LINE, 'utf-8') + b'\r\n')
+    DIR = LINE.split()[1]
+    REGISTER_MAYUSC = LINE.split()[0].upper()
+    print(REGISTER_MAYUSC)
+    if LINE.split()[0] == 'register':
+        LINE = REGISTER_MAYUSC + ' ' + DIR + ' SIP/2.0\r\n' #me falla barra nj
+    my_socket.send(bytes('REGISTER sip: ' + LINE + ' SIP/2.0\r\n', 'utf-8'))
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
