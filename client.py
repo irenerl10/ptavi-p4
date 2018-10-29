13e90439fe58f86740b6ae6b9d0e1de5f24d9872
irenerl10 <irene17a@gmail.com>
2018-10-29 09:36:01 +0100

    añadido el Expires

--- client.py
+++ client.py
@@ -7,10 +7,14 @@ Programa cliente UDP que abre un socket a un servidor
 import socket
 import sys
 
+if len(sys.argv) < 6:
+	sys.exit('Usage: client.py ip puerto register sip_addres expires_value')
+
 # Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 LINE = ' '.join(sys.argv[3:])
+EXPIRES = LINE.split()[2]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
@@ -18,10 +22,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     print("Enviando:", LINE)
     DIR = LINE.split()[1]
     REGISTER_MAYUSC = LINE.split()[0].upper()
-    print(REGISTER_MAYUSC)
+    EXP = 'Expires: ' + EXPIRES + '\r\n'
     if LINE.split()[0] == 'register':
-        LINE = REGISTER_MAYUSC + ' ' + DIR + ' SIP/2.0\r\n' #me falla barra nj
-    my_socket.send(bytes('REGISTER sip: ' + LINE + ' SIP/2.0\r\n', 'utf-8'))
+        LINE = REGISTER_MAYUSC + ' ' + DIR + ' SIP/2.0\r\n'
+    my_socket.send(bytes('sip: ' + LINE + EXP, 'utf-8')) 
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
