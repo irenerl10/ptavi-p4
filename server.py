bc55b4d61512c050ea702bd7dfd12553687fc860
irenerl10 <irene17a@gmail.com>
2018-10-31 18:57:11 +0100

    ya casi esta

--- server.py
+++ server.py
@@ -6,6 +6,9 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
+from datetime import datetime, date, timedelta
+import time
+import json
 
 
 if len(sys.argv) != 2:
@@ -18,14 +21,34 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
     users = {}
 
+    def register2json(self):
+        with open('registered.json', 'w') as jsonfile:
+            json.dump(self.users, jsonfile, indent=3)
+
+    def json2registered(self):
+        try:
+            with open('registered.json', 'r') as jsonfile:
+                self.users = jsonload(jsonfile)
+        except(NameError, FileNotFoundError, AttributeError):
+                pass
+
+    def time_expired(self):
+        list = []
+        FECHA_HORA = datetime.now().strftime('%H:%M:%S %d-%m-%Y')
+        for USER in self.users:
+            if self.users[USER]['expires'] <= FECHA_HORA:
+                list.append(USER)
+        for USER in list:
+            del self.users[USER]
+
     def handle(self):
         """
         handle method of the server class
         (all requests will be handled by this method)
         """
+        self.json2registered()
         for line in self.rfile:
             LINE =  line.decode('utf-8').split()
-            print(LINE)
             IP_CLIENT = self.client_address[0]
             PORT_CLIENT = self.client_address[1]
             print("El cliente nos manda ", line.decode('utf-8'))
@@ -33,19 +56,24 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                 USER = LINE[2]
                 self.wfile.write(b'SIP 2.0 OK\r\n\r\n')
             if LINE[0] == 'Expires:':
-                TIME_EXP = line.decode('utf-8').split()[1] 
-                 
-                print(TIME_EXP)
+                TIME_EXP = line.decode('utf-8').split()[1]
                 if TIME_EXP != '0':
-                    self.users[USER] = [IP_CLIENT, TIME_EXP]
+                    FORMATO = "%Y-%m-%d %H:%M:%S"
+                    TIME_ACT = time.gmtime(time.time())
+                    TIME_STR = time.strftime(time.strftime(FORMATO, TIME_ACT))
+                    exp_time = datetime.now() + timedelta(seconds=int(TIME_EXP))
+                   
+                    self.users[USER] = {
+                              'address': IP_CLIENT,
+                              'expires': exp_time.strftime('%H:%M:%S %d-%m-%Y')}
                 elif TIME_EXP == '0':
                     try:
                         del self.users[USER]
                         self.wfile.write(b'SIP 2.0 OK\r\n\r\n')
                     except:
                         print('Unregistered user')
-                    print(self.users)
-        print(self.users)           
+        self.time_expired()       
+        self.register2json()
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001 
